a996c15c23a5c2baf28fb58743891cc9959bc893
binggu <441282413@qq.com>
2022-06-21 18:09:55 +0800

    修改module name

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
-package grpc_go_study
+package main
 
+import (
+    pb "github.com/BingguWang/grpc-go-study/scoreService/proto"
+    service "github.com/BingguWang/grpc-go-study/scoreService/service"
+    "google.golang.org/grpc"
+    "net"
+)
 
-func main(){
+const address = ":50002"
 
-
+// 启动积分项目
+func main() {
+    listener, err := net.Listen("tcp", address)
+    if err != nil {
+        panic(err)
+    }
+    server := grpc.NewServer()
+    // 注册服务
+    pb.RegisterScoreServiceServer(server, &service.ScoreServiceImpl{})
+    if err := server.Serve(listener); err != nil {
+        panic(err)
+    }
 }
